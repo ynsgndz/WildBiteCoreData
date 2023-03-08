eb19e8ada5ebf8889fa62c176e4355b442ed275c
ynsgndz <yns3gndz@hotmail.com.tr>
2023-03-08 14:13:40 +0300

    first file uploaded

---
+++ WildBite/WildBite/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WildBite
+//
+//  Created by Yunus Gunduz on 8.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
